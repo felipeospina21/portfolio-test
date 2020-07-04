4878b94c4370958020477ff5e30d0b3470d1e8ac
Felipe Ospina <felipe.ospina@MINEROS.COM.CO>
2020-07-04 15:20:42 -0500

    Parallax added

--- assets/app.js
+++ assets/app.js
@@ -4,3 +4,14 @@ const toggleBtn = document.querySelector('.nav-toggle');
 toggleBtn.addEventListener('click', () => {
 	links.classList.toggle('show-links');
 });
+
+let parallax = document.querySelector('.parallax');
+let banner = document.querySelector('.banner');
+// const section = document.querySelector('.section');
+
+window.addEventListener('scroll', function() {
+	let scroll = window.pageYOffset;
+
+	parallax.style.transform = `translate3d(0px, ${scroll * -0.4}px, 0px)`;
+	banner.style.transform = `translate3d(0px, ${scroll * -2}px, 0px)`;
+});
